de743c1523eaf18fa276da822c153bec9d5f87e2
sin9932 <sin9932@gmail.com>
2026-02-21 23:50:44 +0900

    맵백업

---
+++ client/map backup/start_beacon.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="start_beacon" tilewidth="128" tileheight="64" tilecount="56" columns="8">
+ <image source="D:/repos/project_outrage/Project-Outrage/client/asset/sprite/map/start_beacon_128x64.png" width="1024" height="448"/>
+ <tile id="0">
+  <properties>
+   <property name="kind" value="start"/>
+   <property name="spawn" value="conyard"/>
+   <property name="team" type="int" value="1"/>
+  </properties>
+ </tile>
+</tileset>
