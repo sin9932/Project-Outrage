5c25cd01f60f8bbc6785380c70ab749cbb77fada
sin9932 <sin9932@gmail.com>
2026-02-21 17:21:04 +0900

    s

---
+++ client/asset/sprite/map/ore.tsx
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ore" tilewidth="128" tileheight="88" spacing="2" tilecount="10" columns="2">
+ 
+ 
+ <image source="D:/repos/project_outrage/Project-Outrage/client/asset/sprite/map/ore.png" width="260" height="450"/>
+ 
+ 
+ <tile id="0">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="gem"/>
+   <property name="stage" type="int" value="1"/>
+   <property name="valueMul" type="int" value="2"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="1000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="1">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="gem"/>
+   <property name="stage" type="int" value="2"/>
+   <property name="valueMul" type="int" value="2"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="2000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="2">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="gem"/>
+   <property name="stage" type="int" value="3"/>
+   <property name="valueMul" type="int" value="2"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="3000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="3">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="gem"/>
+   <property name="stage" type="int" value="4"/>
+   <property name="valueMul" type="int" value="2"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="4000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="4">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="gem"/>
+   <property name="stage" type="int" value="5"/>
+   <property name="valueMul" type="int" value="2"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="5000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="5">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="ore"/>
+   <property name="stage" type="int" value="1"/>
+   <property name="valueMul" type="int" value="1"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="1000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="6">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="ore"/>
+   <property name="stage" type="int" value="2"/>
+   <property name="valueMul" type="int" value="1"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="2000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="7">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="ore"/>
+   <property name="stage" type="int" value="3"/>
+   <property name="valueMul" type="int" value="1"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="3000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="8">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="ore"/>
+   <property name="stage" type="int" value="4"/>
+   <property name="valueMul" type="int" value="1"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="4000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+ 
+ <tile id="9">
+  
+  
+  <properties>
+   <property name="kind" type="string" value="ore"/>
+   <property name="stage" type="int" value="5"/>
+   <property name="valueMul" type="int" value="1"/>
+   <property name="amountMax" type="int" value="5000"/>
+   <property name="amount" type="int" value="5000"/>
+   <property name="harvestRate" type="int" value="10"/>
+   <property name="regen" type="int" value="1"/>
+  </properties>
+ </tile>
+ 
+
+</tileset>
